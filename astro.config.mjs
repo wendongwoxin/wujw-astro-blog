b700bcfe1abadcc0f49beb94a848caf358d49d5a
wendongwoxin <1844287682@qq.com>
2025-01-15 15:10:07 +0800

    vercel init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	output: 'server',
+	adapter: vercel(),
 });
